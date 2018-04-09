56f2f18bd91b1228f56634a2dd0896f6628c9a33
Joe Ellis <joe@joeellis.la>
2018-04-09 14:59:06 -0500

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExTicketUtils.Mixfile do
   def project do
     [
       app: :ex_ticket_utils,
-      version: "0.5.6",
+      version: "0.6.0",
       elixir: "~> 1.4",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
