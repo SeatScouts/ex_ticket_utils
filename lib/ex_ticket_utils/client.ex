c0d1a0f9030233464bed87bf5ae84b99236f3d21
Joe Ellis <joe@joeellis.la>
2018-04-09 13:30:01 -0500

    Remove sandbox, fix incorrect 400 error

--- lib/ex_ticket_utils/client.ex
+++ lib/ex_ticket_utils/client.ex
@@ -32,36 +32,55 @@ defmodule ExTicketUtils.Client do
 
   defp handle_response(response, options) do
     case response do
-      {:ok, %Response{body: body, status_code: 200}} -> Poison.decode(body)
+      {:ok, %Response{body: body, status_code: 200}} ->
+        Poison.decode(body)
+
       {:ok, response = %Response{status_code: 400}} ->
         case Poison.decode(response.body) do
-          {:ok, json} ->
-            message = json["Message"]
+          {:ok, %{"Message" => message}} ->
+            case options[:version] do
+              "v2" ->
+                cond do
+                  Regex.match?(~r/Result not found/, message) ->
+                    {:ok,
+                     %{
+                       "Items" => [],
+                       "Pagination" => %{
+                         "CurrentPage" => 1,
+                         "TotalPages" => 1,
+                         "TotalResults" => 0
+                       }
+                     }}
 
-            if message do
-              case options[:version] do
-                "v2" ->
-                  cond do
-                    Regex.match?(~r/Result not found/, message) -> {:ok, %{"Items" => [], "Pagination" => %{"CurrentPage" => 1, "TotalPages" => 1, "TotalResults" => 0}}}
-                    true -> {:error, :bad_request, response}
-                  end
-                "v3" ->
-                  cond do
-                    Regex.match?(~r/Invalid/, message) -> {:ok, %{"Items" => [], "Records" => 0, "TotalPages" => 1}}
-                    true -> {:error, :bad_request, response}
-                  end
-              end
-            else
-              {:error, :bad_request, response}
+                  true ->
+                    {:error, :bad_request, response}
+                end
+
+              _ ->
+                {:error, :bad_request, response}
             end
-          {:error, _reason} -> {:error, :bad_request, response}
+
+          {:error, reason} ->
+            {:error, :bad_request, %{"reason" => reason}}
         end
-      {:ok, response = %Response{status_code: 403}} -> {:error, :forbidden, response}
-      {:ok, response = %Response{status_code: 404}} -> {:error, :not_found, response}
-      {:ok, response = %Response{status_code: 429}} -> {:error, :too_many_requests, response}
-      {:ok, response = %Response{status_code: 500}} -> {:error, :internal_server_error, response}
-      {:error, raw = %HTTPoison.Error{reason: reason}} -> {:error, reason, raw}
-      {_, response} -> {:error, :unknown, response}
+
+      {:ok, response = %Response{status_code: 403}} ->
+        {:error, :forbidden, response}
+
+      {:ok, response = %Response{status_code: 404}} ->
+        {:error, :not_found, response}
+
+      {:ok, response = %Response{status_code: 429}} ->
+        {:error, :too_many_requests, response}
+
+      {:ok, response = %Response{status_code: 500}} ->
+        {:error, :internal_server_error, response}
+
+      {:error, raw = %HTTPoison.Error{reason: reason}} ->
+        {:error, reason, raw}
+
+      {_, response} ->
+        {:error, :unknown, response}
     end
   end
 
@@ -76,7 +95,7 @@ defmodule ExTicketUtils.Client do
     url = build_url(path, version)
 
     if options[:debug] do
-      IO.inspect [
+      IO.inspect(
         api_token: creds.api_token,
         api_secret: creds.api_secret,
         url: url,
@@ -84,79 +103,76 @@ defmodule ExTicketUtils.Client do
         body: body,
         headers: headers,
         options: options
-      ]
+      )
     end
 
     request(type, url, body, headers, options)
   end
 
   defp process_params(params, :get, path) do
-    path = cond do
-      Enum.empty?(params)  -> path
-      URI.parse(path).query -> path <> "&" <> URI.encode_query(params)
-      true                 -> path <> "?" <> URI.encode_query(params)
-    end
+    path =
+      cond do
+        Enum.empty?(params) -> path
+        URI.parse(path).query -> path <> "&" <> URI.encode_query(params)
+        true -> path <> "?" <> URI.encode_query(params)
+      end
 
     {"", path}
   end
+
   defp process_params(params, :post, path), do: {Poison.encode!(params), path}
   defp process_params(params, :put, path), do: {Poison.encode!(params), path}
 
   defp build_url(path, version) do
     url = Application.get_env(:ex_ticket_utils, :url, nil)
-    is_sandbox = Application.get_env(:ex_ticket_utils, :sandbox, true)
 
     case url do
       nil ->
-        host = cond do
-          is_sandbox == true || is_sandbox == "true" ->
-            case version do
-              "v1" -> "apiv2.ticketutilssandbox.com"
-              "v2" -> "apiv2.ticketutilssandbox.com"
-              _ -> "api.ticketutilssandbox.com"
-            end
-          is_sandbox == false || is_sandbox == "false" ->
-            case version do
-              "v1" -> "api.ticketutils.net"
-              "v2" -> "api.ticketutils.net"
-              _ -> "api.ticketutils.com"
-            end
-        end
+        host =
+          case version do
+            "v1" -> "api.ticketutils.net"
+            "v2" -> "api.ticketutils.net"
+            _ -> "api.ticketutils.com"
+          end
 
         URI.to_string(%URI{scheme: "https", host: host, path: path})
 
       url ->
-        url = url
-        |> URI.parse
+        url
+        |> URI.parse()
         |> Map.put(:path, path)
-        |> URI.to_string
+        |> URI.to_string()
     end
   end
 
   defp process_headers(creds, path, version) do
-    extras = case version do
-      "v1" -> ["X-API-Version": 1]
-      "v2" -> ["X-API-Version": 2]
-      "v3" -> ["X-API-Version": 3]
-      _ -> []
-    end
+    extras =
+      case version do
+        "v1" -> ["X-API-Version": 1]
+        "v2" -> ["X-API-Version": 2]
+        "v3" -> ["X-API-Version": 3]
+        _ -> []
+      end
 
     %{api_token: api_token} = creds
 
     signature = encode_request(creds, path)
 
-    Keyword.merge([
-      "X-Signature": signature,
-      "X-Token": api_token,
-      "Content-Type": "application/json; charset=utf-8",
-      "Connection": "Keep-Alive",
-      "Accept": "application/json"
-    ], extras)
+    Keyword.merge(
+      [
+        "X-Signature": signature,
+        "X-Token": api_token,
+        "Content-Type": "application/json; charset=utf-8",
+        Connection: "Keep-Alive",
+        Accept: "application/json"
+      ],
+      extras
+    )
   end
 
   def encode_request(creds, path) do
     %{api_secret: api_secret} = creds
 
-    :crypto.hmac(:sha256, api_secret, path) |> Base.encode64
+    :crypto.hmac(:sha256, api_secret, path) |> Base.encode64()
   end
 end
